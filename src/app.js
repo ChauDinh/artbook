38dfd81d63a60a20e186f3025391691535c48dde
Chau Dinh <chaudvb95uc@gmail.com>
2020-06-21 12:25:44 +0700

    layout

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
 import exphbs from "express-handlebars";
+import path from "path";
 
 import router from "./routes/index";
 import { connectDB } from "./config/db";
@@ -23,6 +24,9 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "main" }));
 app.set("view engine", ".hbs");
 app.set("views", "./src/views");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api/v1/", router);
